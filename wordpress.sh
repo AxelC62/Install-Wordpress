bacf630ad16317d79dcd9cbe9f767afa73b0b71f
Axel Cht <79381645+AxelC62@users.noreply.github.com>
2023-12-21 22:10:39 +0100

    Add files via upload

---
+++ wordpress.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+apt update -y
+apt install apache2 -y
+apt install zip -y
+apt install mariadb-server -y
+apt install php -y
+apt install php-mysql -y
+apt install wget -y
+
+cd /tmp
+wget https://wordpress.org/latest.zip
+
+echo "Veuillize saisir le nom de votre bsd"
+read bsd
+echo "Veuilliez saisir votre user"
+read usr
+echo "Veuilliez saisir votre mdp"
+read mdp
+mysql --user=root --password=btsinfo -e "CREATE DATABASE $bsd; CREATE USER '$usr'@'localhost' IDENTIFIED BY '$mdp'; GRANT ALL PRIVILEGES ON $bsd.* TO $usr@localhost; FLUSH PRIVILEGES;"
+rm /var/www/html/index.html
+unzip latest.zip -d /var/www/html
+cd /var/www/html
+mv wordpress/* /var/www/html/
+rm wordpress/ -Rf
+chown -R www-data:www-data /var/www/html/
+#sudo find /var/www/html/ -type f -exec chmod 644 {}\;
+#sudo find /var/www/html/ -type d -exec chmod 755 {}\;
+systemctl restart apache2
+
+
+
+
+
+
+
+
